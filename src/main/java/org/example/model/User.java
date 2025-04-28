c4ac9ae89f35e1ce9aa3bf5ee33a4c87364fbeda
Sergey Rogozhkin <r8damik@gmail.com>
2025-04-28 15:45:33 +0400

    Revert "Заменил Data на Getter и Setter в User Entity"
    This reverts commit 9102208d85537b183dc9c8c211f24c14d01f527e.

--- src/main/java/org/example/model/User.java
+++ src/main/java/org/example/model/User.java
@@ -1,16 +1,13 @@
 package org.example.model;
 
 
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-@Getter
-@Setter
+@Data
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
@@ -32,23 +29,6 @@ public class User {
         this.name = name;
         this.email = email;
         this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) &&
-                Objects.equals(name, user.name) &&
-                Objects.equals(email, user.email) &&
-                Objects.equals(age, user.age) &&
-                Objects.equals(createdAt, user.createdAt);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, email, age, createdAt);
     }
 }
 
